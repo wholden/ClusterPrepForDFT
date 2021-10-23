890d4f8b6918bba6e2a74362fdc7b99266c32c3a
easyXAFS <wholden@users.noreply.github.com>
2021-10-23 08:38:17 +0100

    hmm, need to change approach; commit to save temporary efforts

--- ClusterPrepForDFT/backend.py
+++ ClusterPrepForDFT/backend.py
@@ -7,16 +7,44 @@ class MolecularCluster(mg.core.Molecule):
     
     def __init__(self, species, coords, **kwargs):
         super().__init__(species, coords, **kwargs)
+
+    @staticmethod
+    def process_structure_to_molecular_cluster(structure, cluster_radius, center_atom):
+        index = structure.species.index(mg.core.Element(center_atom))
+        sites = structure.get_sites_in_sphere(structure[index].coords, cluster_radius)
+        sites = [s[0] for s in sites] # drop calculated distances
+        new = MolecularCluster.from_sites(sites, skip_process=True)
+        new.translate_sites(vector=-structure[index].coords)
+        new._structure = mg.core.Structure.from_sites(sites)
+        new._structure.translate_sites(indices=range(len(sites)), vector=-structure[index].coords, frac_coords=False, to_unit_cell=False)
+        return new
         
     @classmethod
     def from_file(cls, filename, cluster_radius, center_atom):
         structure = mg.core.Structure.from_file(filename)
-        index = structure.species.index(mg.core.Element(center_atom))
-        sites = structure.get_sites_in_sphere(structure[index].coords, cluster_radius)
-        sites = [s[0] for s in sites] # drop calculated distances
-        mol = cls.from_sites(sites)
-        mol.translate_sites(vector=-structure[index].coords)
-        return mol
+        new = cls.process_structure_to_molecular_cluster(structure, cluster_radius, center_atom)
+        return new
+
+    @classmethod
+    def from_dict(cls, d, cluster_radius, center_atom):
+        structure = mg.core.Structure.from_dict(d)
+        new = cls.process_structure_to_molecular_cluster(structure, cluster_radius, center_atom)
+        return new
+
+    @classmethod
+    def from_sites(cls, sites, cluster_radius=0, center_atom='', skip_process=False):
+        if not skip_process:
+            structure = mg.core.Structure.from_sites(sites)
+            new = cls.process_structure_to_molecular_cluster(structure, cluster_radius, center_atom)
+        else:
+            new = super().from_sites(sites)
+        return new
+
+    @classmethod
+    def from_str(cls, string, cluster_radius, center_atom):
+        structure = mg.core.Structure.from_str(string)
+        new = cls.process_structure_to_molecular_cluster(structure, cluster_radius, center_atom)
+        return new
         
     def display_jupyter(self, center_color='rgb(3,248,252)'):
         import nglview
